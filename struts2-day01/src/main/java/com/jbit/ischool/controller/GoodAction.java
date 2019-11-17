4a29cfc38bccf9f8ecc455c730a551a1807fba6b
tracy <912884004@qq.com>
2019-11-17 14:16:07 +0800

    update GoodsAction

---
+++ struts2-day01/src/main/java/com/jbit/ischool/controller/GoodAction.java
@@ -0,0 +1,15 @@
+package com.jbit.ischool.controller;
+/**
+ *   处理和商品相关请求的控制器
+ * @author Administrator
+ *
+ */
+public class GoodAction {
+	/**
+	 * 	根据编号查询商品信息
+	 * @return
+	 */
+	public String findGoodsById() {
+		return "suc";
+	}
+}
